a2e3dd4fc7f3152c6e54903d126060961248f157
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-02 13:48:35 -0500

    Update Product.js

--- store/src/components/Product.js
+++ store/src/components/Product.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { ProductConsumer } from '../context';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
+import PropTypes from "prop-types";
 
 export default class Product extends Component {
   render() {
+    // destructuring
+    // pull values out of product
     const { id, title, img, price, inCart } = this.props.product;
 
     return (
@@ -22,6 +24,7 @@ export default class Product extends Component {
                 </Link>
                 <button
                   className="cart-btn"
+                  // btn disabled if item is in cart already
                   disabled={inCart ? true : false}
                   onClick={() => {
                     value.addToCart(id);
@@ -53,15 +56,18 @@ export default class Product extends Component {
   }
 }
 
+// define prop types
 Product.propTypes = {
   product: PropTypes.shape({
     id: PropTypes.number,
     img: PropTypes.string,
     title: PropTypes.string,
     price: PropTypes.number,
-    inCart: PropTypes.bool,
-  }).isRequired,
+    inCart: PropTypes.bool
+  }).isRequired
 };
+
+// styled component
 
 const ProductWrapper = styled.div`
   .card {
